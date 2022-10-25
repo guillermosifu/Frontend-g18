49b91e1bb9f906b0428d7992cd0d86c6ba7858e0
guillermosifu <sifuentes1201@gmail.com>
2022-10-24 23:09:15 -0500

    prueba morir

---
+++ semana5/dia1/calculadora/index.js
@@ -0,0 +1,12 @@
+
+const documentReady = ()=>{
+    const calculatorForm = document.getElementById('calculatorForm');
+
+    const calculate = (e) =>{
+        e.preventDefault();
+        const number1 = +document.getElementById('number1')
+        const operator = +document.getElementById('operator')
+        const number2 = +document.getElementById('number2')
+        const result = +document.getElementById('result')
+    }
+}
