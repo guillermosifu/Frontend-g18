da0d08bfdbc1907d77f378b81450118a573ef004
guillermosifu <sifuentes1201@gmail.com>
2022-11-02 22:37:13 -0500

    swal alert

--- semana6/dia3/events.js
+++ semana6/dia3/events.js
@@ -76,3 +76,75 @@ btnAlertBotones.onclick = function(){
 
     })
 }
+
+ const btnAlertaImagen =document.querySelector("#btn-alerta-imagen")
+const urlMarvel = "https://www.cinemascomics.com/wp-content/uploads/2022/02/villanos-marvel-comics-960x720.jpg"
+ btnAlertaImagen.onclick = function(){
+    Swal.fire({
+        title: "Marvel",
+        text: "alerta con imagen",
+        imageUrl: urlMarvel,
+        imageWidth : 200,
+        iamgeHight : 200,
+
+    })
+ }
+
+ const btnAlertaCustom= document.querySelector("#btn-alerta-custom")
+
+ btnAlertaCustom.onclick = function(){
+    Swal.fire({
+        title: "cutom Alert",
+        text : "Alerta personalizada",
+        background : `url(${urlMarvel})`,
+        color: "#fff",
+        backdrop:
+        `rgba(0,0,0,0.3)
+        url(https://media.giphy.com/media/xT5LMHxhOfscxPfIfm/giphy.gif) left top  no-repeat`,
+    })
+ }
+
+ const btnAlertinputs = document.querySelector("#btn-alerta-inputs")
+
+ btnAlertinputs.onclick = function(){
+    Swal.fire({
+        title: "input para email",
+        input:"email",
+        inputLabel : "ingrese su correo",
+        inputPlaceholder: "el correo debe ser valido",    
+    }).then((resultado)=>{
+        //obnetgo el resultado de lo quye eescribo el usuario
+        console.log(resultado.value)
+    })
+ }
+
+ const paises = document.querySelector("#select-opciones")
+ //onchange sirve para detectar si hay un cambio enmi elemtno
+
+ paises.onchange = function(event){
+    //de evento quiero captura el value y options
+    const{value,options} = event.target; 
+
+    console.log (value)
+const index = options.selectedIndex;
+console.log("index",index)
+console.log("texto",options[index].text);
+
+}
+
+const usuario ={
+    nombre: "pepe",
+    apellido :"zapata",
+    edad: 30,
+    direccion :{
+        calle: "calle siempre viva",
+        numero: 123,
+        ciudad: "springfield"
+    }
+}
+
+//nosotsot podemos destructura 
+
+const{nombre, apellido , edad ,direccion}= usuario;
+
+console.log(apellido);
