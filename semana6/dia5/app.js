bc343cc8d4f18fe6f7b0e34c6628a68136ff41c1
guillermosifu <sifuentes1201@gmail.com>
2022-11-04 20:24:53 -0500

    todolist

--- semana6/dia5/app.js
+++ semana6/dia5/app.js
@@ -1,7 +1,8 @@
 const formulario = document.getElementById("formulario");
 const input = document.getElementById("input");
-const template = document.getElementById("template");
+const template = document.getElementById("template").content;
 const tareaList = document.getElementById("lista-tareas")
+const fragmento = document.createDocumentFragment();
 
 let tareas = {};
 
@@ -29,6 +30,39 @@ const setTareas=()=>{
 const pintarTareas=()=>{
  tareaList.innerHTML ='';
  Object.values(tareas).forEach(tarea =>{
-    
- })
+    const clone = template.cloneNode(true);
+    clone.querySelector('p').textContent = tarea.texto;
+    if(tarea.estado){
+        clone.querySelector('.alert').classList.replace('alert-success','alert-primary');
+        clone.querySelector('.fas').classList.replace('fa-check-circle','fa-undo-alt');
+        clone.querySelector('p').style.textDecoration="line-through";
+          }
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tareas.id;
+        clone.querySelectorAll('.fas')[1].dataset.id = tareas.id;
+        fragmento.appendChild(clone);
+        tareaList.appendChild(fragmento);
+
+ });
+}
+
+tareaList.addEventListener('click',(e)=>{//El e es pra sederle la accio a acada elemento hijo del contenedor que tiene eventlistener
+    action(e)
+})
+
+const action = e =>{
+    if(e.target.classList.contains('fa-check-circle')){
+        tareas[e.target.dataset.id].estado = true;
+        pintarTareas()
+    }else if(e.target.classList.contains('fa-minus-circle')){
+        delete tareas[e.target.dataset.id];
+        pintarTareas()
+    };
+    if(e.target.classList.contains('fa-undo-alt')){
+        tareas[e.target.Date.id].estado = false
+        pintarTareas();
+    };
+
+    e.stopPropagation;
+
 }
