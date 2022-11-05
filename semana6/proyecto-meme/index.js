9223e2fd76ce2fe9e6916750ee3b3ed7d3193561
guillermosifu <sifuentes1201@gmail.com>
2022-11-04 23:03:11 -0500

    filter

---
+++ semana6/proyecto-meme/index.js
@@ -0,0 +1,14 @@
+
+const listaInterna = ()=>{
+    const imagenes = [
+        {
+            nombre: "ponte la pilas",
+            imagen: "link de la imagen",
+            url: "url de la imagen"
+        }
+    ]
+
+    const buscador = document.getElementById("buscador");
+    const resultado = document.getElementById("resultado");
+
+}
